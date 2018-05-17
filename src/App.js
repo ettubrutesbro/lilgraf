6ad289826e0583c820690a1808fb96dc654993b8
Jack L <jack.leng@gmail.com>
2018-05-17 14:24:57 -0700

    parseFloat, division/suffix of large # ticks

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ const statsToUse = [
   //static ranges allow user-to-user comparisons to hold more value
   //you could make it dynamic to accommodate for crazy outlier values, but the value added
   // of a trend graph is probably to track trends, not outliers and world records, right? 
-  {title: 'Kills / game', datalabel: 'kills', rangeTop: 15, rangeBottom: 0},
-  {title: 'Score', datalabel: 'score', rangeTop: 15000, rangeBottom: 0, divideBy: 1000},
+  {title: 'Kills / game', datalabel: 'kills', rangeTop: 18, rangeBottom: 0},
+  {title: 'Score', datalabel: 'score', rangeTop: 20000, rangeBottom: 0, divideTick: 1000},
   {title: 'Wins', datalabel: 'placetop1', rangeTop: 12, rangeBottom: 0},
   {title: 'Top 5\'s', datalabel: 'placetop5', rangeTop: 15, rangeBottom: 0},
-  {title: 'Top 10\'s', datalabel: 'placetop10', rangeTop: 15, rangeBottom: 0},
-  {title: 'Top 25\'s', datalabel: 'placetop25', rangeTop: 15, rangeBottom: 0},
+  //TODO: using top 12 here and not 10/25 because those are null values only in the provided data
+  {title: 'Top 12\'s', datalabel: 'placetop12', rangeTop: 25, rangeBottom: 0},
+  // {title: 'Top 10\'s', datalabel: 'placetop10', rangeTop: 15, rangeBottom: 0},
+  // {title: 'Top 25\'s', datalabel: 'placetop25', rangeTop: 15, rangeBottom: 0},
   {title: 'Time Played', datalabel: 'minutesPlayed', rangeTop: 720, rangeBottom: 0}
 ]
 
@@ -33,13 +35,13 @@ class TrendGraph extends Component {
 
   render() {
     const {selectedStat} = this.state
-    const {rangeTop, rangeBottom} = statsToUse[selectedStat]
+    const {rangeTop, rangeBottom, divideTick} = statsToUse[selectedStat]
 
     const shownData = data.map((d, i, arr)=>{
       //converts data into per-day, divides if necessary
       const label = statsToUse[selectedStat].datalabel
       let divisor = 1
-      //if kills, divide by game...
+      //if kills, the divisor is games played in the same day...
       if(label === 'kills') divisor = i<arr.length-1? d.stats.matches_played - arr[i+1].stats.matches_played : 1
       return i<arr.length-1? (d.stats[label] - arr[i+1].stats[label]) / divisor : 0
     })
@@ -49,8 +51,13 @@ class TrendGraph extends Component {
     const rangediff = (rangeTop - rangeBottom) / (this.props.yTicks-1)
     for(var i = 0; i<this.props.yTicks; i++){
       let tick = rangeTop - (i*rangediff)
-      if(tick % 1!==0) tick = tick.toFixed(1)
-      yTicks.push(tick)
+      let suffix = ''
+      if(divideTick){
+        tick = tick/divideTick
+        if(divideTick===1000) suffix = 'k' 
+      }
+      if(tick % 1!==0) tick = parseFloat(tick.toFixed(2))
+      yTicks.push(tick + suffix)
     }
 
     return (
